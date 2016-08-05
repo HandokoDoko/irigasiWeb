5c0c54696752e2b1f6eb91d4f42577602b0392e3
handokodoko <dokoabdi@ymail.com>
2016-08-05 15:56:29 +0700

    handoko

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -69,4 +69,13 @@ class AuthController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
+    public function showRegistrationForm()
+    {
+        return redirect('login');
+    }
+
+    public function register()
+    {
+
+    }
 }
